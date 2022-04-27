026554711d09fde5e61b0adc99215118c40b6173
ElMigu17 <rgomiguel17@gmail.com>
2022-04-27 14:05:03 -0300

    onde nãão táá comentado ta compilando. Onde táá comentado, ta dando erro de compilaçãão

--- knn.cpp
+++ knn.cpp
@@ -17,30 +17,37 @@ struct type_info{
 
 class point{
     private:
-        double *attributes = new double [QTD_ATTRIBUTES];
+        double *attributes;
         int type;
-    public:
-    
-    point(double out_attributes[QTD_ATTRIBUTES]){
-        attributes = out_attributes;
-    }
-
-    void set_type(int type){
-        type = type;
-    }
-
-    double get_attribute(int pos){
-        return attributes[pos];
-    }
-
-    double calculate_distance(point other_point){
-        double total = 0;
-        for(int i = 0; i < QTD_ATTRIBUTES; i++){
-            double diference = attributes[i] - other_point.get_attribute(i);
-            total += diference * diference;
+    public: 
+        point(){
+            this->attributes = new double [QTD_ATTRIBUTES];
         }
-        return sqrt(total);
-    }
+
+        void set_attributes(double attributes[QTD_ATTRIBUTES]){
+            this->attributes = attributes;
+        }
+
+        void set_type(int type){
+            this->type = type;
+        }
+
+        int get_type(){
+            return type;
+        }
+
+        double get_attribute(int pos){
+            return attributes[pos];
+        }
+
+        double calculate_distance(point other_point){
+            double total = 0;
+            for(int i = 0; i < QTD_ATTRIBUTES; i++){
+                double diference = attributes[i] - other_point.get_attribute(i);
+                total += diference * diference;
+            }
+            return sqrt(total);
+        }
 };
 
 class list{
@@ -50,37 +57,51 @@ class list{
         point pivo;
 
     public:
-        list(int outter_num_neighbours){
-            num_neighbours = outter_num_neighbours;
+        list(int num_neighbours){
+            this->num_neighbours = num_neighbours;
         }
 
-        void add_pivo(point out_pivo){
-            pivo = out_pivo;
+        void add_pivo(point pivo){
+            this->pivo = pivo;
         }
 
         void add(point new_neighbourd){
-            double distance = this.pivo.calculate_distance(new_neighbourd);
+            double distance = this->pivo.calculate_distance(new_neighbourd);
 
-            if(my_list.size() != 0 && distance > my_list[my_list.size()].calculate_distance(pivo)){
-                return void;
+            if(this->my_list.size() == 0 || distance > this->my_list[this->my_list.size()].calculate_distance(pivo)){
+                if(this->my_list.size() == this->num_neighbours){
+                    this->my_list.pop_back();
+                }
+                this->incert_ordered_in_list(new_neighbourd);
             }
-            if(vector.size() == this.num_neighbours){
-                my_list.pop_back();
-            }
-            my_list.push_back(new_neighbourd);
-            sort(my_list.begin(), my_list.end());
         }
 
+        void incert_ordered_in_list(point new_neighbourd){
+            int i = this->my_list.size()-1;
+            double new_distance = this->pivo.calculate_distance(new_neighbourd);
+            while(i >= 0){
+                if(new_distance > this->pivo.calculate_distance(this->my_list[i])){
+                   this->my_list.insert(this->my_list.begin() + i+1 ,new_neighbourd);
+                   break;
+                }
+                i--;
+            }
+            if(i == -1){
+                this->my_list.insert(this->my_list.begin(), new_neighbourd);
+                
+            }
+        }
+/*
         void define_type(){
             vector<type_info> species_present;
 
-            for (int i = 0; i < my_list.size(); i++){
-                int pos = this.pos(species_present, my_list[i].type);
+            for (int i = 0; i < this->my_list.size(); i++){
+                int pos = this.pos(species_present, this->my_list[i].type);
                 if( pos == -1){
                     type_info new_type_info;
-                    new_type_info.type = my_list[i].type;
+                    new_type_info.type = this->my_list[i].type;
                     new_type_info.qtd = 1;
-                    species_present.push_back(my_list[i].type);
+                    species_present.push_back(this->my_list[i].type);
                 }
                 else{
                     species_present[pos].qtd++;
@@ -96,7 +117,7 @@ class list{
 
         }
 
-        int pos(vector<type_info> species_present, int target){
+        int pos(int target, vector<type_info> species_present){
             for (int i = 0; i < species_present.size(); i++){
                 if(species_present[i].type == target){
                     return i;
@@ -104,11 +125,11 @@ class list{
             }
             return -1;
         }
-
+*/
         int count(int target){
             int qtd = 0;
-            for (int i = 0; i < my_list.size(); i++){
-                if(my_list[i] == target){
+            for (int i = 0; i < this->my_list.size(); i++){
+                if(this->my_list[i].get_type() == target){
                     qtd++;
                 }
             }
@@ -123,20 +144,23 @@ int main(){
     random_shuffle(lines.begin(), lines.end());
     int porcentage = 70;
     int qtd_train = (lines.size()/10)*7;
-
-
+    int pos_type = QTD_ATTRIBUTES;
 
     for(int i = 0; i < qtd_train; i++){
-        cout << i << " " << lines[i].size() << "\n";
-        //shuffle(i_vec1.begin(), i_vec1.end(), rng);
+        
         double* att = new double [QTD_ATTRIBUTES];
         for(int j = 0; j < QTD_ATTRIBUTES - 1 ; j++){
-            att[j] = lines[i][j];
+            att[j] = lines[0][j];
         }
-        point new_point(att);
-        int new_point_type = (int)lines[i][lines.size()-1];
+        point new_point;
+        new_point.set_attributes(att);
+        int new_point_type = (int)lines[0][pos_type];
         new_point.set_type(new_point_type);
         list_of_point.push_back(new_point);
-
+        lines.erase(lines.begin());
+    }
+    cout << "\n\n ------------------------------------------------ \n\n ";
+    for(int i = 0; i < list_of_point.size(); i++){
+        cout << i << "-"<<list_of_point[i].get_type() << " ";
     }
 }
